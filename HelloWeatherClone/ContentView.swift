cfe582e463edb01c28e9bdb48ee64f15be5fb397
Kirill Sheremetev <85827294+Rote-cloud@users.noreply.github.com>
2024-03-06 12:52:06 +0600

    add base project

--- HelloWeatherClone/ContentView.swift
+++ HelloWeatherClone/ContentView.swift
@@ -1,21 +1,10 @@
-//
-//  ContentView.swift
-//  HelloWeatherClone
-//
-//  Created by Kirill Sheremetev on 06.03.2024.
-//
-
 import SwiftUI
 
 struct ContentView: View {
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+            
         }
-        .padding()
     }
 }
 
