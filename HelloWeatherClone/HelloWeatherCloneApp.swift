dbeac75f2099ea924aa30f2577d2ed9c98853f0e
Kirill Sheremetev <85827294+Rote-cloud@users.noreply.github.com>
2024-03-06 12:30:27 +0600

    Initial Commit

---
+++ HelloWeatherClone/HelloWeatherCloneApp.swift
@@ -0,0 +1,17 @@
+//
+//  HelloWeatherCloneApp.swift
+//  HelloWeatherClone
+//
+//  Created by Kirill Sheremetev on 06.03.2024.
+//
+
+import SwiftUI
+
+@main
+struct HelloWeatherCloneApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
